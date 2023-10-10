8a82cbf078565cf00f47d07da3bd83fffc85617f
Gustavo Neris <nerisgs20@gmail.com>
2023-10-10 11:51:34 -0300

    feat: add routes of post and get with id

--- src/main/java/com/lespattes/apilespattes/controllers/UserController.java
+++ src/main/java/com/lespattes/apilespattes/controllers/UserController.java
@@ -1,23 +1,38 @@
 package com.lespattes.apilespattes.controllers;
 
+import com.lespattes.apilespattes.domain.User;
+import com.lespattes.apilespattes.dtos.UserDTO;
+import com.lespattes.apilespattes.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import com.lespattes.apilespattes.domain.UserRepository;
+import com.lespattes.apilespattes.repositories.UserRepository;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
 	@Autowired
-	private UserRepository repository;
+	private UserService userService;
+
 	@GetMapping
-	public ResponseEntity getAllUsers() {
-		var response = repository.findAll();
+	public ResponseEntity<List<User>> getUsers() {
+		var response = userService.getUsers();
+		return new ResponseEntity<>(response, HttpStatus.OK);
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity findUserById(@PathVariable String id) throws Exception {
+		var response = userService.findUserById(id);
 		return ResponseEntity.ok(response);
+	}
+	@PostMapping
+	public ResponseEntity<User> createUser(@RequestBody UserDTO user) {
+		var newUser = userService.createUser(user);
+		return new ResponseEntity<>(newUser, HttpStatus.OK);
 	}
 
 }
